757b1dc60770a3f9b3c785850a76b3ecdf6330d1
YLoskutov <y.loskutov@mail.ru>
2015-11-22 19:10:33 +0300

    MySqltest

--- test/src/testTest.java
+++ test/src/testTest.java
@@ -11,29 +11,32 @@ import java.sql.Statement;
 public class testTest {
 
     // JDBC URL, username and password of MySQL server
-    private static final String url = "jdbc:mysql://localhost:3306/forJava";
+    private static final String url = "jdbc:mysql://localhost:3306/test";
     private static final String user = "root";
-    private static final String password = "123";
+    private static final String password = "root";
 
     // JDBC variables for opening and managing connection
     private static Connection con;
     private static Statement stmt;
-
+    private static ResultSet rs;
 
     public static void main(String args[]) {
-        String query = "INSERT INTO Books (id, name, author) \\n\" +\n" +
-                "               \" VALUES (3, 'Head First Java', 'Kathy Sieara');";
-
+        String query = "select count(*) from books";
 
         try {
             // opening database connection to MySQL server
             con = DriverManager.getConnection(url, user, password);
 
             // getting Statement object to execute query
-            stmt.executeUpdate(query);
+            stmt = con.createStatement();
 
+            // executing SELECT query
+            rs = stmt.executeQuery(query);
 
-
+            while (rs.next()) {
+                int count = rs.getInt(1);
+                System.out.println("Total number of books in the table : " + count);
+            }
 
         } catch (SQLException sqlEx) {
             sqlEx.printStackTrace();
@@ -41,7 +44,7 @@ public class testTest {
             //close connection ,stmt and resultset here
             try { con.close(); } catch(SQLException se) { /*can't do anything */ }
             try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
-
+            try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
         }
     }
 
